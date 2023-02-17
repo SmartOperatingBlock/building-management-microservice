ea7bff80aecf1a89f430bc2f87b4b9fd5c657ad9
Andrea Giulianelli <andrygiulio@gmail.com>
2023-02-17 11:18:36 +0100

    chore: add medical technology concepts

---
+++ src/main/kotlin/entity/medicaltechnology/MedicalTechnology.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entity.medicaltechnology
+
+import entity.zone.RoomID
+
+/**
+ * Describe a medical technology used inside the operating block.
+ * This is an entity that is identified by an [id].
+ * In addition, it has a [name], a [description] and a [type].
+ * A medical technology can be associated to a [entity.zone.Room] through its [roomId].
+ */
+data class MedicalTechnology(
+    val id: MedicalTechnologyID,
+    val name: String,
+    val description: String,
+    val type: MedicalTechnologyType,
+    val roomId: RoomID? = null,
+) {
+    init {
+        require(name.isNotEmpty())
+        require(description.isNotEmpty())
+    }
+
+    override fun equals(other: Any?): Boolean = when {
+        other === this -> true
+        other is MedicalTechnology -> this.id == other.id
+        else -> false
+    }
+
+    override fun hashCode(): Int = this.id.hashCode()
+}
+
+/**
+ * Identification of a [MedicalTechnology].
+ * @param[value] the id.
+ */
+data class MedicalTechnologyID(val value: String) {
+    init {
+        // Constructor validation: The id must not be empty
+        require(this.value.isNotEmpty())
+    }
+}
+
+/**
+ * The type of [MedicalTechnology].
+ */
+enum class MedicalTechnologyType {
+    /** Endoscope technology. */
+    ENDOSCOPE,
+    /** X-ray technology. */
+    XRAY,
+}
