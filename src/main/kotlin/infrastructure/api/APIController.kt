f24e4ef215406dd20d42f2837881622c556eb2b2
andrea.giulianelli <andrygiulio@gmail.com>
2023-02-20 11:24:55 +0100

    chore: configure ktor content negotiation

--- src/main/kotlin/infrastructure/api/APIController.kt
+++ src/main/kotlin/infrastructure/api/APIController.kt
@@ -8,10 +8,13 @@
 
 package infrastructure.api
 
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
+import io.ktor.server.application.install
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.delete
 import io.ktor.server.routing.get
@@ -27,7 +30,12 @@ class APIController {
      * Starts the http server to serve the client requests.
      */
     fun start() {
-        embeddedServer(Netty, port = 3000, module = this::dispatcher).start(wait = true)
+        embeddedServer(Netty, port = 3000) {
+            dispatcher(this)
+            install(ContentNegotiation) {
+                json()
+            }
+        }.start(wait = true)
     }
 
     /**
