01754ecfdd92676292f4634a8d217ac6ed20a1c1
andrea.giulianelli <andrygiulio@gmail.com>
2023-02-21 18:22:09 +0100

    chore: add room controller

---
+++ src/main/kotlin/application/controller/RoomController.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.controller
+
+import application.controller.manager.RoomDigitalTwinManager
+import entity.zone.Room
+import entity.zone.RoomID
+import usecase.repository.RoomRepository
+import java.util.Date
+
+/**
+ * Implementation of room repository that handle the application logic
+ * using both db and digital twin.
+ * @param[roomDtManager] the digital twin manager of rooms.
+ */
+class RoomController(private val roomDtManager: RoomDigitalTwinManager) : RoomRepository {
+    override fun createRoom(room: Room): Room? =
+        if (this.roomDtManager.createRoomDigitalTwin(room)) room else null
+
+    override fun deleteRoom(roomId: RoomID): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun findBy(roomId: RoomID, dateTime: Date): Room? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getRooms(): Set<Room> {
+        TODO("Not yet implemented")
+    }
+}
