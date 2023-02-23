e3f0efd4cde8f0a60e47cea968c39175ec10b795
Andrea Giulianelli <andrygiulio@gmail.com>
2023-02-23 09:41:17 +0100

    chore: create room database manager interface

---
+++ src/main/kotlin/application/controller/manager/RoomDatabaseManager.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.controller.manager
+
+import entity.zone.Room
+import entity.zone.RoomID
+import java.util.Date
+
+/**
+ * This interface models the manager of the database for rooms.
+ * In this way it is independent respect to the specific technology that is used,
+ * having the only semantic dependency on the concept of db.
+ */
+interface RoomDatabaseManager {
+    /**
+     * Save a [room] inside the DB.
+     * @return true if successfully stored, false otherwise.
+     */
+    fun saveRoom(room: Room): Boolean
+
+    /**
+     * Delete a room identified by its [roomId] from the DB.
+     * @return true if successfully deleted, false otherwise.
+     */
+    fun deleteRoom(roomId: RoomID): Boolean
+
+    /**
+     * Get a room identified by its [roomId] from the DB.
+     * Specify a past [dateTime] in order to get historical data.
+     * @return null if data is not available, the room instead.
+     */
+    fun findBy(roomId: RoomID, dateTime: Date): Room?
+}
