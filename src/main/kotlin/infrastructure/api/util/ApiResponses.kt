944de04506ef64ee43b445e8031b6ebcd34c231e
Andrea Giulianelli <andrygiulio@gmail.com>
2023-02-28 15:10:50 +0100

    chore: add api responses utils to model rest api responses

---
+++ src/main/kotlin/infrastructure/api/util/ApiResponses.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package infrastructure.api.util
+
+import kotlinx.serialization.Serializable
+
+/** Module that wraps the Api Responses in order to cope with the REST API responses. */
+object ApiResponses {
+    /**
+     * Class that represents an [entry] returned as response by an API.
+     * It needs to include the entry's [url].
+     */
+    @Serializable
+    data class ResponseEntry<out T>(val entry: T, val url: String)
+
+    /**
+     * Class that represents a list of [entries] returned as response to an API request.
+     * As the REST API best-practise recommend it is included also the [total] number of the entries.
+     */
+    @Serializable
+    data class ResponseEntryList<out T>(val entries: List<T>, val total: Int = entries.count())
+}
