ffeb276fb87fefa15aad7d8797621121a2ef11e4
Andrea Giulianelli <andrygiulio@gmail.com>
2023-02-28 16:06:22 +0100

    chore: add medical technology digital twin manager

---
+++ src/main/kotlin/application/controller/manager/MedicalTechnologyDigitalTwinManager.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.controller.manager
+
+import entity.medicaltechnology.MedicalTechnology
+import entity.medicaltechnology.MedicalTechnologyID
+import entity.zone.RoomID
+
+/**
+ * This interface models the manager of Digital Twins for medical technologies.
+ * In this way it is independent respect to the specific technology that is used,
+ * having the only semantic dependency on the concept of Digital Twin.
+ */
+interface MedicalTechnologyDigitalTwinManager {
+    /**
+     * Create the Digital Twin of the [medicalTechnology].
+     * @return true if successfully created, false otherwise.
+     */
+    fun createMedicalTechnologyDigitalTwin(medicalTechnology: MedicalTechnology): Boolean
+
+    /**
+     * Delete the Digital Twin of the medical technology identified by [medicalTechnologyId].
+     * @return true if successfully deleted, false otherwise.
+     */
+    fun deleteMedicalTechnologyDigitalTwin(medicalTechnologyId: MedicalTechnologyID)
+
+    /**
+     * Map a medical technology Digital Twin, identified by its [medicalTechnologyId], to a room
+     * Digital Twin identified by its [roomId].
+     * @return true if successfully mapped, false otherwise
+     */
+    fun mapTo(medicalTechnologyId: MedicalTechnologyID, roomId: RoomID): Boolean
+}
