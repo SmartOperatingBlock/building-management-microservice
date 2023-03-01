eb17d90e1a8a60d0424d5a5833ad9a64fff8649e
andrea.giulianelli <andrygiulio@gmail.com>
2023-03-01 11:30:45 +0100

    test: add room services tests

--- src/test/kotlin/application/service/RoomServiceTest.kt
+++ src/test/kotlin/application/service/RoomServiceTest.kt
@@ -9,7 +9,9 @@
 package application.service
 
 import application.controller.RoomController
+import entity.environment.Temperature
 import entity.zone.Room
+import entity.zone.RoomEnvironmentalData
 import entity.zone.RoomID
 import entity.zone.RoomType
 import entity.zone.ZoneID
@@ -19,42 +21,97 @@ import infrastructure.digitaltwins.DigitalTwinManagerTestDouble
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
+import java.time.Instant
+import java.time.temporal.ChronoUnit
 
 class RoomServiceTest : StringSpec({
-    val exampleRoom = Room(RoomID("r1"), RoomType.OPERATING_ROOM, ZoneID("z1"))
+    val exampleRoom = Room(
+        RoomID("r1"),
+        RoomType.OPERATING_ROOM,
+        ZoneID("z1"),
+        environmentalData = RoomEnvironmentalData(temperature = Temperature(30.3))
+    )
+
+    fun controller() = RoomController(
+        DigitalTwinManagerTestDouble(),
+        DatabaseManager("mongodb://localhost:27017")
+    )
 
     "I should be able to create a room" {
         withMongo {
-            val roomController = RoomController(
-                DigitalTwinManagerTestDouble(),
-                DatabaseManager("mongodb://localhost:27017")
-            )
+            val roomController = controller()
             val result = RoomService.CreateRoom(exampleRoom, roomController).execute()
             result shouldNotBe null
             RoomService.GetRoom(exampleRoom.id, roomController).execute() shouldBe exampleRoom
         }
     }
-//
-//    "I should be able to delete an existing room" {
-//        Service.CreateRoom(exampleRoom, roomController).execute()
-//        val result = Service.DeleteRoom(exampleRoom.id, roomController).execute()
-//        result shouldBe true
-//        Service.GetRoom(exampleRoom.id, roomController).execute() shouldBe null
-//    }
-//
-//    "If a room does not exist, then call delete on it result in a failed request" {
-//        val result = Service.DeleteRoom(exampleRoom.id, roomController).execute()
-//        result shouldBe false
-//    }
-//
-//    "I should be able to get the state of an existing room" {
-//        Service.CreateRoom(exampleRoom, roomController).execute()
-//        val result = Service.GetRoom(exampleRoom.id, roomController).execute()
-//        result shouldBe exampleRoom
-//    }
-//
-//    "If a room does not exist, a request on its state, must return null" {
-//        val result = Service.GetRoom(exampleRoom.id, roomController).execute()
-//        result shouldBe null
-//    }
+
+    "I should not be able to insert a pre-existing room" {
+        withMongo {
+            val roomController = controller()
+            RoomService.CreateRoom(exampleRoom, roomController).execute()
+            RoomService.CreateRoom(exampleRoom, roomController).execute() shouldBe null
+        }
+    }
+
+    "I should be able to delete an existing room" {
+        withMongo {
+            val roomController = controller()
+            RoomService.CreateRoom(exampleRoom, roomController).execute()
+            val result = RoomService.DeleteRoom(exampleRoom.id, roomController).execute()
+            result shouldBe true
+            RoomService.GetRoom(exampleRoom.id, roomController).execute() shouldBe null
+        }
+    }
+
+    "If a room does not exist, then call delete on it result in a failed request" {
+        withMongo {
+            val roomController = controller()
+            RoomService.DeleteRoom(exampleRoom.id, roomController).execute() shouldBe false
+        }
+    }
+
+    "I should be able to get the current state of an existing room" {
+        withMongo {
+            val roomController = controller()
+            RoomService.CreateRoom(exampleRoom, roomController).execute()
+            RoomService.GetRoom(exampleRoom.id, roomController).execute() shouldBe exampleRoom
+        }
+    }
+
+    "I should be able to get the past state of an existing room" {
+        withMongo {
+            val roomController = controller()
+            RoomService.CreateRoom(exampleRoom, roomController).execute()
+            RoomService.GetRoom(exampleRoom.id, roomController, Instant.now().minus(1, ChronoUnit.DAYS))
+                .execute()?.id shouldBe exampleRoom.id // Check only on the id of the room
+        }
+    }
+
+    "If a room does not exist, a request on its state, must return null" {
+        withMongo {
+            val roomController = controller()
+            RoomService.GetRoom(exampleRoom.id, roomController).execute() shouldBe null
+        }
+    }
+
+    "It should be possible to obtain all the room entries" {
+        withMongo {
+            val roomController = controller()
+            // Populate
+            val roomList = listOf(exampleRoom, exampleRoom.copy(id = RoomID("room-1")))
+            roomList.forEach { RoomService.CreateRoom(it, roomController).execute() }
+            val entries = RoomService.GetAllRoomEntry(roomController).execute()
+            // Checks
+            entries.count() shouldBe roomList.count()
+            entries.forEach { roomList.map { room -> room.id.value }.contains(it.id) shouldBe true }
+        }
+    }
+
+    "It should be possible to ask for room entries even where there aren't" {
+        withMongo {
+            val roomController = controller()
+            RoomService.GetAllRoomEntry(roomController).execute().count() shouldBe 0
+        }
+    }
 })
