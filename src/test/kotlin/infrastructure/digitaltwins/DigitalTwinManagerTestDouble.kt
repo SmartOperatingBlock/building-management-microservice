6599d71db35fd63b8e532ec528ffa78fbaadb7d5
Andrea Giulianelli <andrygiulio@gmail.com>
2023-03-05 17:55:59 +0100

    test: add medical technology digital twin manager test double

--- src/test/kotlin/infrastructure/digitaltwins/DigitalTwinManagerTestDouble.kt
+++ src/test/kotlin/infrastructure/digitaltwins/DigitalTwinManagerTestDouble.kt
@@ -8,26 +8,48 @@
 
 package infrastructure.digitaltwins
 
+import application.controller.manager.MedicalTechnologyDigitalTwinManager
 import application.controller.manager.RoomDigitalTwinManager
+import entity.medicaltechnology.MedicalTechnology
+import entity.medicaltechnology.MedicalTechnologyID
 import entity.zone.Room
 import entity.zone.RoomID
 
 /**
  * Simple Test Double (fake) for [RoomDigitalTwinManager] in order to perform tests.
  */
-class DigitalTwinManagerTestDouble : RoomDigitalTwinManager {
-    private val roomList: MutableSet<Room> = mutableSetOf()
+class DigitalTwinManagerTestDouble : RoomDigitalTwinManager, MedicalTechnologyDigitalTwinManager {
+    private val rooms: MutableSet<Room> = mutableSetOf()
+    private val medicalTechnologies: MutableSet<MedicalTechnology> = mutableSetOf()
 
     override fun createRoomDigitalTwin(room: Room): Boolean {
-        this.roomList.add(room)
+        this.rooms.add(room)
         return true
     }
 
-    override fun deleteRoomDigitalTwin(roomId: RoomID): Boolean {
-        return this.roomList.removeIf { it.id == roomId }
+    override fun deleteRoomDigitalTwin(roomId: RoomID): Boolean = this.rooms.removeIf { it.id == roomId }
+
+    override fun findBy(roomId: RoomID): Room? = this.rooms.find { it.id == roomId }
+
+    override fun createMedicalTechnologyDigitalTwin(medicalTechnology: MedicalTechnology): Boolean {
+        this.medicalTechnologies.add(medicalTechnology)
+        return true
     }
 
-    override fun findBy(roomId: RoomID): Room? {
-        return this.roomList.find { it.id == roomId }
-    }
+    override fun deleteMedicalTechnologyDigitalTwin(medicalTechnologyId: MedicalTechnologyID): Boolean =
+        this.medicalTechnologies.removeIf { it.id == medicalTechnologyId }
+
+    override fun findBy(medicalTechnologyId: MedicalTechnologyID): MedicalTechnology? =
+        this.medicalTechnologies.find { it.id == medicalTechnologyId }
+
+    override fun mapTo(medicalTechnologyId: MedicalTechnologyID, roomId: RoomID?): Boolean =
+        with(this.findBy(medicalTechnologyId)) {
+            if (this != null) {
+                this@DigitalTwinManagerTestDouble.deleteMedicalTechnologyDigitalTwin(medicalTechnologyId)
+                this@DigitalTwinManagerTestDouble.createMedicalTechnologyDigitalTwin(this.copy(roomId = roomId))
+                true
+            } else {
+                false
+            }
+        }
 }
