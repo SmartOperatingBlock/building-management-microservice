820adedaedd9659cda0fea96b4b3a2446f707006
Andrea Giulianelli <andrygiulio@gmail.com>
2023-03-08 23:08:36 +0100

    chore: add medical technology kafka event model

---
+++ src/main/kotlin/infrastructure/events/model/MedicalTechnologyEvent.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package infrastructure.events.model
+
+import kotlinx.serialization.Serializable
+
+/** Medical Technology update event. */
+@Serializable
+data class MedicalTechnologyEvent(
+    override val key: String,
+    override val data: MedicalTechnologyUsagePayload,
+    override val dateTime: String
+) : Event<MedicalTechnologyUsagePayload>
+
+/**
+ * [MedicalTechnologyEvent] payload that refers to the usage described by the [isInUse] property of
+ * a medical technology identified by its [medicalTechnologyID].
+ */
+@Serializable
+data class MedicalTechnologyUsagePayload(val medicalTechnologyID: String, val isInUse: Boolean)
+
+/** Module that wraps the possible keys for a [MedicalTechnologyEvent]. */
+object MedicalTechnologyEventKey {
+    /** Medical Technology usage update event. */
+    const val USAGE_EVENT = "MEDICAL_TECHNOLOGY_USAGE_EVENT"
+}
