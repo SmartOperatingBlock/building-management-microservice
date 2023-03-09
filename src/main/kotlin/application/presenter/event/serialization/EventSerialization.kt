708c858b6a09cb318e8e20504633130a3cf94dfa
andrea.giulianelli <andrygiulio@gmail.com>
2023-03-09 14:09:30 +0100

    chore: add serialization of external events

--- src/main/kotlin/application/presenter/event/serialization/EventSerialization.kt
+++ src/main/kotlin/application/presenter/event/serialization/EventSerialization.kt
@@ -8,6 +8,11 @@
 
 package application.presenter.event.serialization
 
+import application.presenter.event.model.Event
+import application.presenter.event.model.MedicalTechnologyEvent
+import application.presenter.event.model.MedicalTechnologyEventKey
+import application.presenter.event.model.RoomEvent
+import application.presenter.event.model.RoomEventKey
 import application.presenter.event.model.RoomEventPayloads
 import entity.environment.Humidity
 import entity.environment.LightUnit
@@ -15,9 +20,24 @@ import entity.environment.Luminosity
 import entity.environment.Presence
 import entity.environment.Temperature
 import entity.environment.TemperatureUnit
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 
 /** Module that wraps the event serialization. */
 object EventSerialization {
+    /**
+     * Convert an event body to an [Event] object giving its [eventKey].
+     * @throws IllegalArgumentException if the event cannot be deserialized.
+     */
+    fun String.toEvent(eventKey: String): Event<*> = when (eventKey) {
+        RoomEventKey.TEMPERATURE_EVENT -> Json.decodeFromString<RoomEvent<RoomEventPayloads.TemperaturePayload>>(this)
+        RoomEventKey.HUMIDITY_EVENT -> Json.decodeFromString<RoomEvent<RoomEventPayloads.HumidityPayload>>(this)
+        RoomEventKey.LUMINOSITY_EVENT -> Json.decodeFromString<RoomEvent<RoomEventPayloads.LuminosityPayload>>(this)
+        RoomEventKey.PRESENCE_EVENT -> Json.decodeFromString<RoomEvent<RoomEventPayloads.PresencePayload>>(this)
+        MedicalTechnologyEventKey.USAGE_EVENT -> Json.decodeFromString<MedicalTechnologyEvent>(this)
+        else -> throw IllegalArgumentException("Event not supported")
+    }
+
     /** Convert a Temperature Payload to [Temperature]. */
     fun RoomEventPayloads.TemperaturePayload.toTemperature() =
         Temperature(this.temperatureValue, this.temperatureUnit.toTemperatureUnit())
