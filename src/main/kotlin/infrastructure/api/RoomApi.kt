5bac204135dd53fbe05e1cb3520dc6a88eb6f8ac
Andrea Giulianelli <andrygiulio@gmail.com>
2023-03-21 14:34:54 +0100

    refactor: split api in route extension function

--- src/main/kotlin/infrastructure/api/RoomApi.kt
+++ src/main/kotlin/infrastructure/api/RoomApi.kt
@@ -23,6 +23,7 @@ import io.ktor.server.application.call
 import io.ktor.server.request.receive
 import io.ktor.server.response.header
 import io.ktor.server.response.respond
+import io.ktor.server.routing.Route
 import io.ktor.server.routing.delete
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
@@ -37,54 +38,66 @@ import java.time.Instant
  */
 fun Application.roomAPI(apiPath: String, port: Int, provider: ManagerProvider) {
     routing {
-        post("$apiPath/rooms") {
-            val room = call.receive<RoomEntry>().toRoom()
-            RoomService.CreateRoom(
-                room,
-                RoomController(provider.roomDigitalTwinManager, provider.roomDatabaseManager)
-            ).execute().apply {
-                when (this) {
-                    null -> call.respond(HttpStatusCode.Conflict)
-                    else -> {
-                        call.response.header(
-                            HttpHeaders.Location,
-                            "http://localhost:$port$apiPath/rooms/${room.id.value}"
-                        )
-                        call.respond(HttpStatusCode.Created)
-                    }
-                }
-            }
-        }
-        get("$apiPath/rooms") {
-            val entries = RoomService.GetAllRoomEntry(
-                RoomController(provider.roomDigitalTwinManager, provider.roomDatabaseManager)
-            ).execute().map { entry ->
-                ApiResponses.ResponseEntry(entry, "http://localhost:$port$apiPath/rooms/${entry.id}")
-            }
-            call.response.status(if (entries.isNotEmpty()) HttpStatusCode.OK else HttpStatusCode.NoContent)
-            call.respond(ApiResponses.ResponseEntryList(entries))
-        }
-        get("$apiPath/rooms/{roomId}") {
-            RoomService.GetRoom(
-                RoomID(call.parameters["roomId"].orEmpty()),
-                RoomController(provider.roomDigitalTwinManager, provider.roomDatabaseManager),
-                call.request.queryParameters["dateTime"]?.let { rawDateTime -> Instant.parse(rawDateTime) }
-            ).execute().apply {
-                when (this) {
-                    null -> call.respond(HttpStatusCode.NotFound)
-                    else -> call.respond(this.toRoomApiDto())
-                }
-            }
-        }
-        delete("$apiPath/rooms/{roomId}") {
-            call.respond(
-                RoomService.DeleteRoom(
-                    RoomID(call.parameters["roomId"].orEmpty()),
-                    RoomController(provider.roomDigitalTwinManager, provider.roomDatabaseManager)
-                ).execute().let { result ->
-                    if (result) HttpStatusCode.NoContent else HttpStatusCode.NotFound
-                }
-            )
-        }
+        createRoom(apiPath, port, provider)
+        getAllRooms(apiPath, port, provider)
+        getRoom(apiPath, provider)
+        deleteRoom(apiPath, provider)
     }
 }
+
+private fun Route.createRoom(apiPath: String, port: Int, provider: ManagerProvider) =
+    post("$apiPath/rooms") {
+        val room = call.receive<RoomEntry>().toRoom()
+        RoomService.CreateRoom(
+            room,
+            RoomController(provider.roomDigitalTwinManager, provider.roomDatabaseManager)
+        ).execute().apply {
+            when (this) {
+                null -> call.respond(HttpStatusCode.Conflict)
+                else -> {
+                    call.response.header(
+                        HttpHeaders.Location,
+                        "http://localhost:$port$apiPath/rooms/${room.id.value}"
+                    )
+                    call.respond(HttpStatusCode.Created)
+                }
+            }
+        }
+    }
+
+private fun Route.getAllRooms(apiPath: String, port: Int, provider: ManagerProvider) =
+    get("$apiPath/rooms") {
+        val entries = RoomService.GetAllRoomEntry(
+            RoomController(provider.roomDigitalTwinManager, provider.roomDatabaseManager)
+        ).execute().map { entry ->
+            ApiResponses.ResponseEntry(entry, "http://localhost:$port$apiPath/rooms/${entry.id}")
+        }
+        call.response.status(if (entries.isNotEmpty()) HttpStatusCode.OK else HttpStatusCode.NoContent)
+        call.respond(ApiResponses.ResponseEntryList(entries))
+    }
+
+private fun Route.getRoom(apiPath: String, provider: ManagerProvider) =
+    get("$apiPath/rooms/{roomId}") {
+        RoomService.GetRoom(
+            RoomID(call.parameters["roomId"].orEmpty()),
+            RoomController(provider.roomDigitalTwinManager, provider.roomDatabaseManager),
+            call.request.queryParameters["dateTime"]?.let { rawDateTime -> Instant.parse(rawDateTime) }
+        ).execute().apply {
+            when (this) {
+                null -> call.respond(HttpStatusCode.NotFound)
+                else -> call.respond(this.toRoomApiDto())
+            }
+        }
+    }
+
+private fun Route.deleteRoom(apiPath: String, provider: ManagerProvider) =
+    delete("$apiPath/rooms/{roomId}") {
+        call.respond(
+            RoomService.DeleteRoom(
+                RoomID(call.parameters["roomId"].orEmpty()),
+                RoomController(provider.roomDigitalTwinManager, provider.roomDatabaseManager)
+            ).execute().let { result ->
+                if (result) HttpStatusCode.NoContent else HttpStatusCode.NotFound
+            }
+        )
+    }
